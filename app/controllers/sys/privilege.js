ab07e8584713f634192dfa18b55268f722a727e1
chenlingjie <chenlingjie@cd121.com>
2017-07-14 16:43:41 +0800

    基础完成

--- app/controllers/sys/privilege.js
+++ app/controllers/sys/privilege.js
@@ -1,55 +1,67 @@
 /**
  * Created by xiaobxia on 2017/6/30.
  */
-// const BaseResult = require('../../model/result/baseResult');
-// const sessionConst = require('../../model/const/session');
-// const privilegeService = require('../../service/privilegeService');
-//
-// //分析父子关系，生成菜单
-// function createMenu(menus) {
-//   let directory = [];
-//   let menuMap = {};
-//   for (let k = 0; k < menus.length; k++) {
-//     let menu = menus[k];
-//     if (menu['TYPE'] === 0) {
-//       directory.push({
-//         id: menu['PRIV_ID'],
-//         name: menu['PRIV_NAME']
-//       });
-//     } else if (menu['TYPE'] === 1) {
-//       if (menuMap[menu['PARENT_PRIV_ID']] === undefined) {
-//         menuMap[menu['PARENT_PRIV_ID']] = [];
-//       }
-//       menuMap[menu['PARENT_PRIV_ID']].push({
-//         id: menu['PRIV_ID'],
-//         name: menu['PRIV_NAME'],
-//         url: menu['URL'],
-//         path: menu['PATH']
-//       })
-//     }
-//   }
-//   for (let j = 0; j < directory.length; j++) {
-//     directory[j]['children'] = menuMap[directory[j]['id']]
-//   }
-//   return directory;
-// }
-// /**
-//  * method get
-//  * api sys/priv/menu
-//  * @param res
-//  * @param req
-//  * @param next
-//  */
-// exports.menu = function (req, res, next) {
-//   let result = new BaseResult();
-//   let user = req.session[sessionConst.SESSION_LOGIN_USER];
-//   privilegeService.getUserMenu(user['USER_ID'], function (error, menus) {
-//     if (error) {
-//       result.setErrorCode(error.code);
-//       result.setErrorMessage(error.message);
-//     } else {
-//       result.setResult(createMenu(menus))
-//       res.json(result)
-//     }
-//   })
-// };
+const co = require('co');
+const BaseController = require('../base');
+const PrivilegeService = require('../../service/privilegeService');
+
+module.exports = class PrivilegeController extends BaseController{
+  /**
+   * method get
+   * api sys/priv/menu
+   * @param res
+   * @param req
+   * @param next
+   */
+  menu () {
+    let self = this;
+    return co.wrap(function*(req, res, next) {
+      let user = self.getSessionUser(req.session);
+      let connection = null;
+      let result = self.result();
+      try {
+        connection = yield self.getPoolConnection();
+        let privilegeService = new PrivilegeService(connection);
+        let menus = yield privilegeService.getUserMenu(user['USER_ID']);
+        connection.release();
+        result.setResult(createMenu(menus));
+        res.json(result);
+      } catch (error) {
+        if (connection) {
+          connection.release();
+        }
+        result.setErrorCode(error.code);
+        result.setErrorMessage(error.message);
+        res.json(result);
+      }
+    });
+  }
+};
+
+function createMenu(menus) {
+  let directory = [];
+  let menuMap = {};
+  for (let k = 0; k < menus.length; k++) {
+    let menu = menus[k];
+    if (menu['TYPE'] === 0) {
+      directory.push({
+        id: menu['PRIV_ID'],
+        name: menu['PRIV_NAME']
+      });
+    } else if (menu['TYPE'] === 1) {
+      if (menuMap[menu['PARENT_PRIV_ID']] === undefined) {
+        menuMap[menu['PARENT_PRIV_ID']] = [];
+      }
+      menuMap[menu['PARENT_PRIV_ID']].push({
+        id: menu['PRIV_ID'],
+        name: menu['PRIV_NAME'],
+        url: menu['URL'],
+        path: menu['PATH']
+      });
+    }
+  }
+  for (let j = 0; j < directory.length; j++) {
+    directory[j]['children'] = menuMap[directory[j]['id']]
+  }
+  return directory;
+}
--- app/routes/sys.js
+++ app/routes/sys.js
@@ -2,15 +2,12 @@
  * Created by xiaobxia on 2017/7/5.
  */
 const express = require('express');
-// const login = require('../controllers/sys/login');
-// const priv = require('../controllers/sys/privilege');
-// const user = require('../controllers/sys/user');
-// const test = require('../controllers/test');
 const UserController = require('../controllers/sys/user');
-let userController = new UserController();
 const LoginController = require('../controllers/sys/login');
+const PrivilegeController = require('../controllers/sys/privilege');
+let userController = new UserController();
 let loginController = new LoginController();
-
+let privilegeController = new PrivilegeController();
 let router = express.Router();
 
 // router.get('/sys/test', test.test);
@@ -19,13 +16,12 @@ let router = express.Router();
 router.post('/sys/login', loginController.login());
 router.get('/sys/isLogin', loginController.isLogin());
 router.get('/sys/logout', loginController.logout());
-// //权限
-// router.get('/sys/priv/menu', priv.menu);
-//
+//权限
+router.get('/sys/priv/menu', privilegeController.menu());
+// 用户
 router.post('/sys/user/checkUserMenuPriv', userController.checkUserMenuPriv());
 router.post('/sys/user/changePwd', userController.changePwd());
 router.get('/sys/user/usersCount',  userController.getUserCount());
-// router.get('/sys/user/:id', user.showUser);
 router.get('/sys/users', userController.getUsers());
 router.get('/sys/user/:id', userController.getUser());
 
