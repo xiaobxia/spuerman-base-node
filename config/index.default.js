3b9b4a28289390b4048157541a8e228ee9941913
chenlingjie <chenlingjie@cd121.com>
2017-08-07 09:36:23 +0800

    修改配置文件

--- config/index.default.js
+++ config/index.default.js
@@ -4,27 +4,31 @@
 const path = require('path');
 module.exports = {
   project: {
+    //务必修改
     projectName: ""
   },
   server: {
-    //后续这个参数由命令行提供
+    //默认
     debug: true,
+    root: path.resolve(__dirname, '../'),
     port: 8080,
     //务必修改
-    session_secret: "secret",
-    root: path.resolve(__dirname, '../')
+    session_secret: "secret"
   },
   //mysql配置
   mysql: {
+    //默认
+    connectionLimit: 10,
+    dateStrings: true,
+    //务必修改
     host: "",
     user: "",
     password: "",
     database: "",
-    connectionLimit: 10,
-    //使返回的date为字符串
-    dateStrings: true
+
   },
   logger: {
+    //默认
     dir: path.resolve(__dirname, '../logs/'),
     fileName: 'cheese.log',
     debugLogLevel: 'ALL',
