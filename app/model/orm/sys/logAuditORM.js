6cb1bc7428415fc86269966ee051dd4188f40680
chenlingjie <chenlingjie@cd121.com>
2017-08-28 17:36:46 +0800

    修改日志审计查询bug

--- app/model/orm/sys/logAuditORM.js
+++ app/model/orm/sys/logAuditORM.js
@@ -25,7 +25,7 @@ module.exports = class LogAuditORM extends BaseORM {
   getLogsByIds(ids) {
     return this.query({
       sql: `SELECT ?? FROM ${this.table} AS la LEFT JOIN SYS_USER AS u ON u.USER_ID=la.USER_ID WHERE ID IN (?)`,
-      values: [['la.*', 'u.USER_ID'], ids]
+      values: [['la.*', 'u.USER_NAME'], ids]
     });
   }
 
